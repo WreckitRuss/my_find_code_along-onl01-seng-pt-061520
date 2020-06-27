0a461b2385d81e76daf8c31abe56bfd42ba8c024
Russell Doane <russdoane@yahoo.com>
2020-06-27 08:49:50 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,7 +5,7 @@ def my_find(collection)
   while i < collection.length
   return collection[i]
    if
-     yield(collection[i])
+     yield(collection[i])== true
     return
     collection[i]
     end
